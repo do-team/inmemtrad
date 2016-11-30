a1393ba2c99aa6cf0dc082d6636e08fab2160a9f
EC2 Default User <ec2-user@ip-172-36-0-88.eu-central-1.compute.internal>
2016-11-30 14:12:01 +0000

    Working socket connection

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 var redis = require("redis"),
     //client = redis.createClient(); //when running on localhost
-    //client = redis.createClient('/tmp/redis.sock'); //when running on local unix socket (fastest possible)
-    client = redis.createClient(6379,'35.156.118.89'); //when running remotely
+    client = redis.createClient('/tmp/redis.sock'); //when running on local unix socket (fastest possible)
+    //client = redis.createClient(6379,'35.156.118.89'); //when running remotely
 // if you'd like to select database 3, instead of 0 (default), call
 // client.select(3, function() { /* ... */ });
 
