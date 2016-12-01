ff1227d037a2dab5fce94dc251a55bbd28d452aa
kubedav <davesade.42@gmail.com>
2016-12-01 14:13:07 +0100

    Random generator working.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // inmemtrad redis nodejs
+var crypto = require("crypto");
+var biguint = require('biguint-format');
 
 var redis = require("redis"),
     //client = redis.createClient(); //when running on localhost
-    client = redis.createClient('/tmp/redis.sock'); //when running on local unix socket (fastest possible)
-    //client = redis.createClient(6379,'35.156.118.89'); //when running remotely
+    //client = redis.createClient('/tmp/redis.sock'); //when running on local unix socket (fastest possible)
+    client = redis.createClient(6379,'35.156.118.89'); //when running remotely
 // if you'd like to select database 3, instead of 0 (default), call
 // client.select(3, function() { /* ... */ });
 
@@ -12,14 +14,16 @@ client.on("error", function (err) {
 });
 
 // testing function
-var randomNo = () => Math.floor(Number.MAX_SAFE_INTEGER * Math.random());
-console.log(randomNo);
+
+function randomPrice (qty) {
+    return crypto.randomBytes(qty);
+}
+console.log(biguint(randomPrice(1), 'dec'));
+
 client.set("zkouska", "BEZ HODNOTY");
 client.get("zkouska", function(err, reply) {
      console.log(reply);
      });
-
-
 
 // end of test
 
