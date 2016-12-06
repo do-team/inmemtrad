e9e0e62dd2c5ce7516bcb6ce8eedecf87cf0e16a
kubedav <davesade.42@gmail.com>
2016-12-06 14:59:06 +0100

    RC1 ! Working as designed... testing now.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// inmemtrad redis nodejs async
 var redis = require("redis");
 var async = require("async");
 var connection = process.env.IMTCONNECT;
@@ -18,56 +17,40 @@ switch (connection) {
         return;
 }
 
-// if you'd like to select database 3, instead of 0 (default), call
-// client.select(3, function() { /* ... */ });
+// if you'd like to select database 3, instead of 0 (default), call client.select(3, function() { /* ... */ });
 
 client.on("error", function(err) {
-    console.log("Error " + err);
+    console.log("Error: " + err);
 });
-
-// testing function
 
 // List of items
 var products = Array("wood", "gold", "fish", "wine");
 var customers = Array("AUX1", "BFG2", "CRE3", "DRS4", "EFI5", "FRA6", "GOR1", "AAR1");
 var ordertypes = Array("buy", "sell");
 
-// Main loop generating random orders
+// Main function
 
-var cycles;
-for (cycles = 0; cycles < 10; cycles++) {
-    async.waterfall([
-        randomisation,
-        evaluation,
-    ], function(err, result) {
-        console.log(err + result + 'end'); // result now equals 'done'
-    });
+function dummy(cycles, callback) {
 
-}
-
-function randomisation(callback) {
+    // Randomiser
     var randomPrice = Math.floor((Math.random() * 299) + 1);
     var randomProduct = products[Math.floor(Math.random() * products.length)];
     var randomCustomer = customers[Math.floor(Math.random() * customers.length)];
     var randomOrderType = ordertypes[Math.floor(Math.random() * ordertypes.length)];
-    console.log(randomCustomer + ' ' + randomOrderType + ' ' + randomProduct + ' ' + randomPrice)
-    callback(randomCustomer,randomOrderType,randomProduct,randomPrice);
-}
+    //console.log(randomCustomer + ' ' + randomOrderType + ' ' + randomProduct + ' ' + randomPrice);
 
-function evaluation(randomCustomer,randomOrderType,randomProduct,randomPrice) {
-    console.log('something');
+    // Evaluation
     switch (randomOrderType) {
         case "buy":
             var redisKey = randomProduct + "-" + randomPrice + "-sell";
-            console.log(redisKey);
             client.lrange(redisKey, 0, 0, function(err, reply) {
+
                 //console.log(reply.length + "here");
                 if (reply.length === 0) {
                     client.rpush(redisKey, randomCustomer);
-                    console.log("New order inserted!" + redisKey, + " " + randomCustomer)
+                    console.log("New order inserted! " + redisKey, +" " + randomCustomer)
                 } else {
-                    console.log("Order exists, best offer by: " + reply);
-                    console.log("We got a trade! Removing best offer...")
+                    console.log("TRADE DETECTED! " + randomCustomer + " just traded " + redisKey + ", best offer by: " + reply + ". Removing from orderbook.");
                     client.lpop(redisKey)
                 }
 
@@ -81,20 +64,26 @@ function evaluation(randomCustomer,randomOrderType,randomProduct,randomPrice) {
                 //console.log(reply.length + "here");
                 if (reply.length === 0) {
                     client.rpush(redisKey, randomCustomer);
-                    console.log("New order inserted!" + redisKey, +" " + randomCustomer)
+                    console.log("New order inserted! " + redisKey, +" " + randomCustomer)
                 } else {
-                    console.log("Order exists, best offer by: " + reply);
-                    console.log("We got a trade! Removing best offer...")
+                    console.log("TRADE DETECTED! " + randomCustomer + " just traded " + redisKey + ", best offer by: " + reply + ". Removing from orderbook.");
                     client.lpop(redisKey)
                 }
 
             });
             break;
-
     }
-    callback();
 }
 
-// End of main loop
+// Main loop generating random orders
+
+for (var cycles = 0; cycles < 1000; cycles++) {
+    dummy(cycles, function(response) {
+        console.log("cycles = " + this.cycles + " , response = " + response);
+    }.bind({
+        cycles: cycles
+    }))
+}
+// End of loop
 
 client.quit();
